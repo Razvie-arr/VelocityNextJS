fe995229b18d58e9dfb399666f1e2f05f43e775f
Razvie-arr <vladikbyastinov@gmail.com>
2024-05-29 13:50:44 +0200

    add fetching car details

--- src/app/car/[id]/page.tsx
+++ src/app/car/[id]/page.tsx
@@ -1,5 +1,28 @@
-const CarDetailPage = () => {
-  return <div>Car Detail</div>;
+import prisma from '@/utils/prisma';
+import Link from 'next/link';
+
+const fetchCarDetail = async (id: string) => {
+  return await prisma.car.findUnique({
+    where: {
+      id: id,
+    },
+    include: {
+      model: true,
+      brand: true,
+    },
+  });
+};
+
+const CarDetailPage = async ({ params }: { params: { id: string } }) => {
+  const car = await fetchCarDetail(params.id);
+  return (
+    <div>
+      <Link href={'/'}>Home</Link>
+      <div>{car?.brand.name}</div>
+      <div>{car?.model.name}</div>
+      <div>{car?.description}</div>
+    </div>
+  );
 };
 
 export default CarDetailPage;
