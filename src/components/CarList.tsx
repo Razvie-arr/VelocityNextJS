ab60512598e08f587620a41311cf3b382356bc3e
Razvie-arr <vladikbyastinov@gmail.com>
2024-05-29 13:22:13 +0200

    add homepage with cars

---
+++ src/components/CarList.tsx
@@ -0,0 +1,19 @@
+import { Car } from '@prisma/client';
+import CarItem from '@/components/CarItem';
+
+type Props = {
+  cars: Car[];
+};
+
+const CarList = ({ cars }: Props) => {
+  return (
+    <div>
+      {cars.map((car) => (
+        <CarItem key={car.id} car={car} />
+      ))}
+      ;
+    </div>
+  );
+};
+
+export default CarList;
